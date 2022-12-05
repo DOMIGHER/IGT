0c478c4adad64ec55aef2bcbb3f399c7ca8271dd
DOMIGHER <Domi_ig2000@hotmail.com>
2022-12-04 18:25:41 -0600

    A

--- application/controllers/ClienteC.php
+++ application/controllers/ClienteC.php
@@ -2,7 +2,13 @@
 
 class ClienteC extends CI_Controller{
 
-    public function showcuentacliente(){}
+    public function showcuentacliente($idcliente){
+        $this->load->model('ClienteM');
+        $data['cliente'] = $this->ClienteM->getcuenta();
+        
+        $this->load->view('Cliente/cuenta/vercuentaV',$data);
+
+    }
 
 }
 ?>
--- application/models/ClienteM.php
+++ application/models/ClienteM.php
@@ -1,7 +1,11 @@
 <?php 
 class ClienteM extends CI_Model{
 
+    function getcuenta(){
+        $query = $this->db->get('cliente');
+        return $query->result();
 
+    }
 
     
 }
